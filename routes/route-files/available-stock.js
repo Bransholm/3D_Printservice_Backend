03ed662a51c46b4c3ca0cad4da9dd40511bc1c84
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-12-09 12:07:42 +0100

    test backup connection

--- routes/route-files/available-stock.js
+++ routes/route-files/available-stock.js
@@ -6,33 +6,15 @@ import { InternalServerErrorResponse } from "../router-error-handling/router-err
 
 import { readAvailavleStockItemsQuery } from "../query-files/stock-queries.js";
 
-import {
-  readCatalougeQuery
-} from "../query-files/catalogue-quries.js";
-
 const avialableStockRouter = Router();
 
 // Reads the catalogue data
 avialableStockRouter.get("/", async (request, response) => {
   try {
-    console.log("avialableStockRouter");
     const result = await readAvailavleStockItemsQuery();
     response.json(result);
   } catch (error) {
     InternalServerErrorResponse(error, response);
   }
 });
-
-// Reads the catalogue data
-// avialableStockRouter.get("/", async (request, response) => {
-//   try {
-//     const result = await readCatalougeQuery();
-//     response.json(result);
-//   } catch (error) {
-//     InternalServerErrorResponse(error, response);
-//   }
-// });
-
-
-
 export default avialableStockRouter;
