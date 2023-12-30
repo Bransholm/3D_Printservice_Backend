95a7e33adf9b91cfb0c21fb0f43b4513593d2d63
SirShagalot <124779092+luma0001@users.noreply.github.com>
2023-12-30 12:11:02 +0100

    MinAmountReached is Active

--- routes/query-files/stock-queries.js
+++ routes/query-files/stock-queries.js
@@ -10,7 +10,8 @@ async function readAllStockItemsQuery() {
 // Create catalogue item
 async function createStockItemQuery(request) {
   const body = request.body;
-  const queryString = /*sql*/
+  const queryString =
+    /*sql*/
     `INSERT INTO stock (Name, Material, Colour, GramInStock, MinAmountReached, SalesPrice) VALUES (?, ?, ?, ?, ?, ?);`;
   const values = [
     body.name,
@@ -27,7 +28,7 @@ async function createStockItemQuery(request) {
 
 // Get catalogue item by ID
 async function readStockItemByIdQuery(id, request) {
-  const queryString =/*sql*/ `SELECT * FROM stock WHERE id = ?`;
+  const queryString = /*sql*/ `SELECT * FROM stock WHERE id = ?`;
   const values = [id];
 
   const [result] = await dbConnection.execute(queryString, values);
@@ -35,34 +36,31 @@ async function readStockItemByIdQuery(id, request) {
   return result;
 }
 
-
-
 // Update catalogue item by ID
 async function updateStockItemQuery(id, request) {
-//   const body = request.body;
-//   // Update query
-//   const queryString =/*sql*/
-//     `UPDATE stock SET Name=?, Material=?, Colour=?, GramInStock=?, MinAmountReached=?, SalesPrice=? WHERE Id=?`;
-//   const values = [
-//     body.name,
-//     body.material,
-//     body.colour,
-//     body.gramInStock,
-//     body.minAmountReached,
-//     body.salesPrice,
-//     id,
-//   ];
-
-//   // Execute the update query within the transaction
-//   const [result] = await dbConnection.query(queryString, values);
-//   return result;
+  //   const body = request.body;
+  //   // Update query
+  //   const queryString =/*sql*/
+  //     `UPDATE stock SET Name=?, Material=?, Colour=?, GramInStock=?, MinAmountReached=?, SalesPrice=? WHERE Id=?`;
+  //   const values = [
+  //     body.name,
+  //     body.material,
+  //     body.colour,
+  //     body.gramInStock,
+  //     body.minAmountReached,
+  //     body.salesPrice,
+  //     id,
+  //   ];
+  //   // Execute the update query within the transaction
+  //   const [result] = await dbConnection.query(queryString, values);
+  //   return result;
 }
 
 // Delete catalogue item by ID
 async function deleteStockItemByIdQuery(id, request) {
   const values = [id];
   // Delete the given item from the catalogue
-  const queryString = /*sql*/`DELETE FROM stock WHERE id=?;`;
+  const queryString = /*sql*/ `DELETE FROM stock WHERE id=?;`;
   const [result] = await dbConnection.query(queryString, values);
   return result;
 }
@@ -70,12 +68,10 @@ async function deleteStockItemByIdQuery(id, request) {
 //Reads asll Materials (where stock minAmountReached is FALSE)
 
 async function readAvailavleStockItemsQuery() {
-  const queryString = /*sql*/ `SELECT * FROM stock WHERE MinAmountReached=0;`;
+  const queryString = /*sql*/ `SELECT * FROM stock WHERE Active=0;`;
   const [result] = await dbConnection.execute(queryString);
   return result;
 }
-
-
 
 export {
   readAllStockItemsQuery,
@@ -83,5 +79,5 @@ export {
   readStockItemByIdQuery,
   updateStockItemQuery,
   deleteStockItemByIdQuery,
-  readAvailavleStockItemsQuery
+  readAvailavleStockItemsQuery,
 };
