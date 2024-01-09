07ef41af264d9e3929cada0cca55ed6f319ed6ce
Bransholm <kenneth@bransholm.dk>
2024-01-09 15:05:46 +0100

    viewOrdrer route

---
+++ routes/route-files/view-ordrer.js
@@ -0,0 +1,22 @@
+import { Router, response } from "express";
+import dbConnection from "../../data-layer/data.js";
+
+const customersRouter = Router();
+
+//Get router for view orders
+viewOrdrerRouter.get("/", async (request, response) => {
+  try {
+      const queryString =
+        /*sql*/
+        `
+        SELECT * from orders
+    `;
+    const [result] = await dbConnection.execute(queryString);
+    response.json(result);
+  } catch (error) {
+    console.error(error);
+    response
+      .status(500)
+      .json({ message: "An Internal Server Error Has Occured" });
+  }
+});
